f43d5292d0ba32d01b383dc25ec3723ac55dd0e1
Rushabh Gosar <rushabh.techie@gmaail.com>
2015-04-18 22:57:19 -0400

    Updating the MainActivity and adding a Genre selector

--- mobile/src/main/java/com/rit/songstack/MainActivity.java
+++ mobile/src/main/java/com/rit/songstack/MainActivity.java
@@ -4,16 +4,85 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import com.larswerkman.holocolorpicker.*;
+import android.util.*;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+    public static final int min = Math.abs(-21760);
+    public static final int max = Math.abs(-16777216);
+    ColorPicker freqSelector;
+    TextView freqView;
+    TextView ampView;
+    ValueBar valueBar;
+    /* Corresponds to the genre codes on the SongGenres.java */
+    private static int _genre = SongGenres.NONE;
+
+    double style = 0d;
+    double amplitude = 0d;
+
+    double Style, Amplitude;
+
+    public static int getGenre(){
+        return _genre;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        freqView = (TextView) findViewById(R.id.textView);
+        freqSelector = (ColorPicker) findViewById( R.id.picker );
+        valueBar = (ValueBar) findViewById(R.id.VB1);
+        ampView = (TextView) findViewById(R.id.textView2);
+
+        freqSelector.addValueBar(valueBar);
+        freqSelector.setOnColorSelectedListener(new ColorPicker.OnColorSelectedListener() {
+            @Override
+            public void onColorSelected(int i) {
+                style = i;
+                Style = updateTempo();
+                String val = ""+ Style;
+                freqView.setText(val.toCharArray(), 0, val.length());
+                Toast.makeText(getApplicationContext(), "" + Style, Toast.LENGTH_SHORT).show();
+            }
+        });
+        freqSelector.setOnColorChangedListener(new ColorPicker.OnColorChangedListener() {
+            @Override
+            public void onColorChanged(int i) {
+                style = i;
+                Style = updateTempo();
+                Toast.makeText(getApplicationContext(), "" + Style, Toast.LENGTH_SHORT).show();
+                String val = ""+ Style;
+                freqView.setText(val.toCharArray(), 0, val.length());
+            }
+        });
+
+        valueBar.setOnValueChangedListener(new ValueBar.OnValueChangedListener() {
+            @Override
+            public void onValueChanged(int i) {
+                amplitude = i;
+                String val = "" + i;
+                ampView.setText(val.toCharArray(), 0, val.length());
+                //updateGenre();
+            }
+        });
+    }
+    public double updateTempo() {
+        if ( style < -1 || style > 1 ){
+            Log.d("SongStack","inside update tempo");
+            style -= min;
+            style = Math.abs(style);
+            double max = this.max - this.min;
+            style = style * 1 / max;
+            Log.d("SongStack", "" + style);
+        }
+        return style;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ mobile/src/main/java/com/rit/songstack/SongGenres.java
@@ -0,0 +1,23 @@
+package com.rit.songstack;
+
+/**
+ * Created by Rushabh on 4/18/2015.
+ */
+public abstract class SongGenres{
+    public static final int NONE = 0 << 0;
+    public static final int ROCK = 1 << 0;
+    public static final int JAZZ = 1 << 1;
+    public static final int CLASSICAL = 1 << 2;
+    public static final int BLUES = 1 << 3;
+    public static final int RnB = 1 << 4;
+    public static final int COUNTRY = 1 << 5;
+    public static final int REGGAE = 1 << 6;
+    public static final int HIPHOP = 1 << 7;
+    public static final int TRANCE = 1 << 8;
+    public static final int LATINO = 1 << 9;
+    public static final int INTERNATIONAL = 1 << 10;
+    public static final int METAL = 1 << 11;
+    public static final int TRIPPY = 1 << 12;
+
+
+}
